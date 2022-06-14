30b3299542f36e4b090984700c28ae21786ad111
puppetup <savinohoshizaki@gmail.com>
2022-06-13 23:20:45 -0700

    added html skeleton

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+/*
+API key: 33765ca5db55155419754b2e9b44f487
+Weather URL: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+Geocode URL: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+*/
+
