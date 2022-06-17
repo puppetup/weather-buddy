3c7c8d9bd93af5274e6b2b06b76a6d5608e24ff3
puppetup <savinohoshizaki@gmail.com>
2022-06-16 17:07:06 -0700

    added global var and event listener

--- assets/script.js
+++ assets/script.js
@@ -5,46 +5,62 @@ Geocode URL: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state c
 http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
 */
 
-location = ''
+const searchButton = document.getElementById('submit')
+let key = '33765ca5db55155419754b2e9b44f487'
 
-//Geocode API
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=Los+Angeles&limit=5&appid=33765ca5db55155419754b2e9b44f487')
-    .then(response => response.json())
-    .then(function (data) {
-        var lon = data[0].lon
-        var lat = data[0].lat
-        console.log(lat,lon)
-        callWeatherApi(lat,lon)
-    });
+getInput = function(event) {
+    event.preventDefault()
+    const userInput = document.getElementById('location').value
+    console.log(userInput)
+    getLocation(userInput)
+}
 
-//Weather API
+
+getLocation = function(userInput) {
+    let geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=${key}`
+    
+    //Geocode API
+    fetch(geoUrl)
+        .then(response => response.json())
+        .then(function (data) {
+            var lon = data[0].lon
+            var lat = data[0].lat
+            console.log(lat,lon)
+            callWeather(lat,lon)
+        });
+    }
+
+    //Weather API
 function callWeather(lat,lon) {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=33765ca5db55155419754b2e9b44f487') 
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`
+    fetch(url) 
         .then(response => response.json())
         .then(function (data) {
             console.log(data)
-            var temp = data.current.temp
-            var hum = data.current.humidity
-            var winspd = data.current.wind_speed
-            var uvi = data.current.uvi
-            var main = data.current.weather.main
-            let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${key}`
-            let key = '33765ca5db55155419754b2e9b44f487'
+            
+            
         })
     };
 
+searchButton.addEventListener('click', getInput)
 
+// html = 
+//                     <div class="card">
+//                       <img src="..." class="card-img-top" alt="...">
+//                       <div class="card-body">
+//                         <h5 class="card-title">Day1</h5>
+//                         <p class="card-text">temp</p>
+//                         <p class="card-text">humidity</p>
+//                         <p class="card-text">windspd</p>
+//                         <p class="card-text">UV</p>
+//                         <p class="card-text">date</p>
+//                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+//                       </div>
+//                     </div>
 
-html = 
-                    <div class="card">
-                      <img src="..." class="card-img-top" alt="...">
-                      <div class="card-body">
-                        <h5 class="card-title">Day1</h5>
-                        <p class="card-text">temp</p>
-                        <p class="card-text">humidity</p>
-                        <p class="card-text">windspd</p>
-                        <p class="card-text">UV</p>
-                        <p class="card-text">date</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                      </div>
-                    </div>
+// var temp = data.current.temp
+//                 var hum = data.current.humidity
+//                 var winspd = data.current.wind_speed
+//                 var uvi = data.current.uvi
+//                 var main = data.current.weather.main
+
