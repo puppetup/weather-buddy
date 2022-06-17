ef64a205dfddfcd671c8d7b4c360a3454f678459
puppetup <savinohoshizaki@gmail.com>
2022-06-16 20:47:21 -0700

    got 5 days to display

--- assets/script.js
+++ assets/script.js
@@ -37,30 +37,41 @@ function callWeather(lat,lon) {
         .then(response => response.json())
         .then(function (data) {
             console.log(data)
-            
-            
+            showWeather(data)   
         })
     };
 
+function showWeather(resp) {
+    console.log(resp)
+    let oneDay = document.querySelector('#one-day')
+    let fiveDay = document.querySelector('.card-group')
+    oneDay.innerHTML =  `<div class="card bg-dark text-white">
+                            <img src="http://openweathermap.org/img/wn/${resp.current.weather[0].icon}@2x.png" class="card-img" alt="...">
+                            <div class="card-img-overlay">
+                            <h5 class="card-title"></h5>
+                            <p class="card-text">Temperature: ${resp.current.temp}</p>
+                            <p class="card-text">Humidity: ${resp.current.humidity}</p>
+                            <p class="card-text">Wind Speed: ${resp.current.wind_speed}</p>
+                            <p class="card-text">UV Index: ${resp.current.uvi}</p>
+                            <p class="card-text">Date: ${resp.current.dt}</p>
+                            </div>
+                        </div>`
+    fiveDay.innerHTML = resp.daily.map((day, idx) => {
+        if (idx < 5) {
+           return ` <div class="card">
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Day1</h5>
+                    <p class="card-text">temp${day.temp.day}</p>
+                    <p class="card-text">humidity${day.humidity}</p>
+                    <p class="card-text">windspd${day.wind_speed}</p>
+                    <p class="card-text">UV${day.uvi}</p>
+                    <p class="card-text">date</p>
+                </div>
+            </div>`
+        }
+    }).join('');
+}
+
 searchButton.addEventListener('click', getInput)
-
-// html = 
-//                     <div class="card">
-//                       <img src="..." class="card-img-top" alt="...">
-//                       <div class="card-body">
-//                         <h5 class="card-title">Day1</h5>
-//                         <p class="card-text">temp</p>
-//                         <p class="card-text">humidity</p>
-//                         <p class="card-text">windspd</p>
-//                         <p class="card-text">UV</p>
-//                         <p class="card-text">date</p>
-//                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//                       </div>
-//                     </div>
-
-// var temp = data.current.temp
-//                 var hum = data.current.humidity
-//                 var winspd = data.current.wind_speed
-//                 var uvi = data.current.uvi
-//                 var main = data.current.weather.main
 
